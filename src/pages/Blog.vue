80d49b434cc63965c3abb9582e91a55f02aad175
Spencer Wallace <dude.wallace@gmail.com>
2019-05-15 08:15:52 -0600

    Add shadow to blog post list

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,15 +1,15 @@
 <template>
   <Layout>
-    <ul>
-      <li v-for="{ node } in $page.allBlogPost.edges" :key="node._id">
-        <router-link :to="node.path">
-          <h2 v-html="node.title"/>
-          <g-image :src="node.image"/>
-          <div class="text-grey-200 mb-5" v-html="node.date"/>
-          <div class="text-black" v-html="node.description"/>
-        </router-link>
-      </li>
-    </ul>
+      <ul>
+        <li class="shadow p-2 mb-5" v-for="{ node } in $page.allBlogPost.edges" :key="node._id">
+          <router-link :to="node.path">
+            <h2 v-html="node.title"/>
+            <g-image :src="node.image"/>
+            <div class="text-grey-200 mb-5" v-html="node.date"/>
+            <div class="text-black" v-html="node.description"/>
+          </router-link>
+        </li>
+      </ul>
   </Layout>
 </template>
 
