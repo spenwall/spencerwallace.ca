b0cb3277500f2ede170b0d0b9430f1ac8ef4e554
Spencer <swallace@weddingstar.com>
2019-05-16 11:53:36 -0600

    Add pagination to blog posts

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,21 +1,36 @@
 <template>
   <Layout>
-      <ul>
-        <li class="shadow p-6 mb-5" v-for="{ node } in $page.allBlogPost.edges" :key="node._id">
-          <router-link :to="node.path">
-            <h2 v-html="node.title"/>
-            <g-image :src="node.image"/>
+    <ul>
+      <li class="shadow-lg mb-5" v-for="{ node } in $page.allBlogPost.edges" :key="node._id">
+        <router-link :to="node.path">
+          <h2 class="p-6" v-html="node.title"/>
+          <g-image :src="node.image"/>
+          <div class="p-6">
             <div class="text-grey-200 mb-5" v-html="node.date"/>
             <div class="text-black" v-html="node.description"/>
-          </router-link>
-        </li>
-      </ul>
+          </div>
+        </router-link>
+      </li>
+    </ul>
+    <Pager
+      class="flex justify-between"
+      :info="$page.allBlogPost.pageInfo"
+      prevLabel="< Newer Posts"
+      nextLabel="Older Posts >"
+      firstLabel=""
+      lastLabel=""
+      range=" "
+    />
   </Layout>
 </template>
 
 <page-query>
   query Home ($page: Int) {
-    allBlogPost (page: $page) {
+    allBlogPost (perPage: 4, page: $page) @paginate {
+      pageInfo {
+        totalPages
+        currentPage
+      }
       edges {
         node {
           _id
@@ -31,7 +46,12 @@
 </page-query>
 
 <script>
+import { Pager } from "gridsome";
+
 export default {
+  components: {
+    Pager
+  },
   metaInfo: {
     title: "Hello, world!"
   }
@@ -39,7 +59,6 @@ export default {
 </script>
 
 <style <style scoped>
-
 img {
   width: 100%;
 }
