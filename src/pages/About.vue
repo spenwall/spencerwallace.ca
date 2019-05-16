e223922c34b39b553c67b89fa7688db013f5e344
Spencer Wallace <dude.wallace@gmail.com>
2019-05-15 21:59:24 -0600

    Add about me

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,16 @@
 <template>
   <Layout>
     <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <p>
+      I am a full stack developer that loves to make great web things. I work with PHP, Javascript, HTML, and CSS.
+      I am here to help others become better developers as I do the same. When I am not writing code I am making delicious 
+      food for my family and friends. Sometimes I also play video games.
+    </p>
+    <p>
+      Feel free to contact me for any reason. I am always open to making new connections. Find me on twitter 
+      <a href="https://twitter.com/spenwall">@spenwall</a>, <a href="https://github.com/swallace">github</a> or 
+      just send me an email <a href="mailto:spencer.wallace@outlook.com">spencer.wallace@outlook.com</a>
+    </p>
   </Layout>
 </template>
 
