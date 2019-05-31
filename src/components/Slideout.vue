5e12717ee7ae67b929ec3171ce2122f7cfbbdfd5
Spencer Wallace <dude.wallace@gmail.com>
2019-05-31 17:54:15 -0600

    Add fonts from fontAwesome

--- src/components/Slideout.vue
+++ src/components/Slideout.vue
@@ -1,13 +1,34 @@
 <template>
   <div class="bg-white shadow-lg fixed w-48 h-screen">
     <ul>
-      <li class="mx-10 mb-5">About</li>
-      <li class="mx-10 mb-5">Experience</li>
-      <li class="mx-10 mb-5">Projects</li>
-      <li class="mx-10 mb-5">Skills</li>
-      <li class="mx-10 mb-5">Education</li>
-      <li class="mx-10 mb-5">Contact</li>
-      <li class="mx-10 mb-5">Resume</li>
+      <li class="mx-10 mb-5 flex items-center">
+        <font-awesome class="text-green-500 mr-2" :icon="['fas', 'user']"/>
+        <a>About</a>
+      </li>
+      <li class="mx-10 mb-5 flex items-center">
+        <font-awesome class="text-green-500 mr-2" :icon="['fas', 'folder-open']"/>
+        <a>Projects</a>
+      </li>
+      <li class="mx-10 mb-5 flex items-center">
+        <font-awesome class="text-green-500 mr-2" :icon="['fab', 'twitter']"/>
+        <a>Experience</a>
+      </li>
+      <li class="mx-10 mb-5 flex items-center">
+        <font-awesome class="text-green-500 mr-2" :icon="['fab', 'amazon']"/>
+        <a>Skills</a>
+      </li>
+      <li class="mx-10 mb-5 flex items-center">
+        <font-awesome class="text-green-500 mr-2" :icon="['fab', 'github']"/>
+        <a>Education</a>
+      </li>
+      <li class="mx-10 mb-5 flex items-center">
+        <font-awesome class="text-green-500 mr-2" :icon="['fab', 'github']"/>
+        <a>Contact</a>
+      </li>
+      <li class="mx-10 mb-5 flex items-center">
+        <font-awesome class="text-green-500 mr-2" :icon="['fab', 'github']"/>
+        <a>Resume</a>
+      </li>
     </ul>
   </div>
 </template>
