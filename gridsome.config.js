d5e1f620967378b3eda8275dbaf4aa0254b78c41
Spencer Wallace <dude.wallace@gmail.com>
2019-06-18 22:20:10 -0600

    Added svg loader

--- gridsome.config.js
+++ gridsome.config.js
@@ -42,4 +42,16 @@ module.exports = {
       },
     },
   },
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg')
+    svgRule.uses.clear()
+    svgRule
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+      .options({
+        svgo: {
+          plugins: [{ removeViewBox: false }]
+        }
+      })
+    }
 }
