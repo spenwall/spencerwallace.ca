d187d991a7411259396a793deb6a663188133c73
Spencer Wallace <dude.wallace@gmail.com>
2019-06-19 22:13:49 -0600

    Add education

--- src/components/section/Education.vue
+++ src/components/section/Education.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
     <Section title="Education" color="red">
-      This is my Education
+      <div class="flex justify-center max-w-5xl flex-wrap">
+        <Card title="University of Lethbridge: 2008 - 2012">
+          <ul class="list-disc mx-0 my-4 pl-12 pb-4">
+            <li>Bachelor of Science: Computer Science</li>
+            <li>Bachelor of Management: General Management</li>
+          </ul>
+        </Card>
+      </div>
     </Section>
   </div>
 </template>
 
 <script>
 import Section from "./Section.vue";
+import Card from "../Card";
 
 export default {
   components: {
-    Section
+    Section,
+    Card
   }
 };
 </script>
