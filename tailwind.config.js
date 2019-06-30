e68f9ccdb95f28964c151c3f48cb3d34475b7947
Spencer Wallace <dude.wallace@gmail.com>
2019-06-29 21:13:01 -0600

    Add tailwind transition plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,5 +19,14 @@ module.exports = {
     }
   },
   variants: {},
-  plugins: []
+  plugins: [
+    require('tailwindcss-transition')({
+      standard: 'all .3s ease',
+      transitions: {
+        'slow': 'all 2s ease',
+        'normal-in-out-quad': 'all 2s cubic-bezier(0.455, 0.03, 0.515, 0.955)',
+        'slow-in-out-quad': 'all 2s cubic-bezier(0.455, 0.03, 0.515, 0.955)',
+      }  
+    })
+  ]
 };
