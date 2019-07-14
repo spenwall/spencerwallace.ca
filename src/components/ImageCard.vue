c7aab8687bffc7586438790f1b343e098cdd87d8
Spencer Wallace <dude.wallace@gmail.com>
2019-07-13 23:34:32 -0600

    Added projects

--- src/components/ImageCard.vue
+++ src/components/ImageCard.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
-    <div @click="openInfo" class="flex flex-col w-64 relative">
+    <div @click="openInfo" class="shadow flex flex-col relative">
       <div
         class="transition absolute z-10 w-12 h-12 top-0 border-4 border-white hover:bg-blue-800 right-0 m-2 flex justify-center items-center rounded-full"
         :class="color"
       >
         <font-awesome class="text-white" :icon="['fas', 'question']" />
       </div>
-      <div class="relative">
+      <div class="relative flex justify-center items-center h-64">
         <g-image :src="image" alt="project"/>
         <transition name="info-transition">
-          <div v-if="info" class="bg-white absolute top-0 w-full h-full p-3 overflow-hidden">
-            This app scrapes the redflagdeals.com website and sends notifications of new deals matching keywords
+          <div v-if="info" class="bg-white absolute top-0 pr-12 w-full h-full p-3 overflow-hidden">
+            <slot></slot>
           </div>
         </transition>
       </div>
-      <div :class="color" class="min-h-4 text-white p-4">
+      <div :class="color" class="min-h-4 text-white p-4 z-10">
         <div v-html="title"></div>
       </div>
     </div>
